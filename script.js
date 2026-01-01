18327674d1d540986e9b51b23efa6f605a0d50e8
rajsumank76-ship-it <rajsumank76@gmail.com>
2026-01-01 08:51:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿// Scroll animation for sections
+window.addEventListener("scroll", () => {
+    document.querySelectorAll("section").forEach(section => {
+        let top = section.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+            section.style.transition = "all 0.8s ease";
+        }
+    });
+});
